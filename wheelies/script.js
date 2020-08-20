fd25fad10f8a4eb1f3adf1063cc9ce2ab5ef20dc
David Colls <dcolls@dcmbp.local>
2020-08-20 17:22:43 +1000

    test sensors

--- wheelies/script.js
+++ wheelies/script.js
@@ -1,7 +1,29 @@
 console.log('Hello Wheelies');
 
 async function loadWam() {
-  model = await tf.loadLayersModel('https://theknack.pro/wheelies/accel_model');
+  model = await tf.loadLayersModel('https://theknack.pro/wheelies/accel_model/model.json');
 }
 
-document.addEventListener('DOMContentLoaded', loadWam);
+
+function updateDisplay(id, value){
+  if (value != null)
+    document.getElementById(id).innerHTML = value.toFixed(3);
+}
+
+function handleMotion(event) {
+  updateDisplay('acc_x', event.acceleration.x);
+}
+
+function startSensors() {
+  if (
+    DeviceMotionEvent &&
+    typeof DeviceMotionEvent.requestPermission === "function"
+  ) {
+    DeviceMotionEvent.requestPermission();
+  }
+  
+  window.addEventListener("devicemotion", handleMotion);
+}
+
+document.addEventListener('DOMContentLoaded', startSensors);
+//document.addEventListener('DOMContentLoaded', loadWam);
