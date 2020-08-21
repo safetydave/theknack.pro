b37134e6fac3f11a775858f6ac70a830c0f97967
David Colls <dcolls@dcmbp.local>
2020-08-21 18:22:49 +1000

    wire up model

--- model/script.js
+++ model/script.js
@@ -1,7 +1,16 @@
+var model;
 
 async function loadWam() {
   model = await tf.loadLayersModel('https://theknack.pro/model/h5js/model.json');
   //model = await tf.loadLayersModel('h5js/model.json');
+}
+
+function predict() {
+    console.log('predicting');
+    x = tf.tensor([[0, 0, 0, 0, 0, 0]]);
+    y = model.predict(x);
+    console.log(y.shape);
+    console.log(y.arraySync()[0])
 }
 
 document.addEventListener('DOMContentLoaded', loadWam);
