9dcca25f3ade85faef5e875c8297b09b44b67941
David Colls <dcolls@dcmbp.local>
2020-08-21 16:27:51 +1000

    another attempt for model

--- model/script.js
+++ model/script.js
@@ -1,7 +1,7 @@
 
 async function loadWam() {
-  //model = await tf.loadLayersModel('https://theknack.pro/model/h5js/model.json');
-  model = await tf.loadLayersModel('h5js/model.json');
+  model = await tf.loadLayersModel('https://theknack.pro/model/h5js/model.json');
+  //model = await tf.loadLayersModel('h5js/model.json');
 }
 
 document.addEventListener('DOMContentLoaded', loadWam);
