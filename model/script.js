6a7978b2426209503c69956d73bc3c0010c28ac9
David Colls <dcolls@dcmbp.local>
2020-08-22 08:10:38 +1000

    pass accel values to model at training interval

--- model/script.js
+++ model/script.js
@@ -7,23 +7,45 @@ async function loadWam() {
 
 function predict() {
     console.log('predicting');
-    x = tf.tensor([[0, 0, 0, 0, 0, 0]]);
+    x_arr = acc_prev.concat(acc_now);
+    x = tf.tensor([x_arr]);
     y = model.predict(x);
-    console.log(y.shape);
-    console.log(y.arraySync()[0])
+    updateDisplay('y', y.arraySync()[0], 2)
+    console.log()
 }
 
-function updateDisplay(id, value){
+var acc_now = [0, 0, 0]
+var acc_prev = [0, 0, 0]
+
+function updateDisplay(id, value, precision=1){
   if (value != null)
-    document.getElementById(id).innerHTML = value.toFixed(1);
+    document.getElementById(id).innerHTML = value.toFixed(precision);
 }
+
+var sample_interval = 100;
+var ts_count = 0;
+var ts_prev;
 
 function handleMotion(event) {
+  ts_now = Date.now();
+  acc = event.acceleration;
+  if (ts_count == 0) {
+    acc_now = [acc.x, acc.y, acc.z];
+    ts_prev = ts_now;
+    ++ts_count;
+  }
+  else {
+    if (ts_now - ts_prev >= sample_interval) {
+        acc_prev = acc_now;
+        acc_now = [acc.x, acc.y, acc.z];
+        ts_prev = ts_now;
+        ++ts_count;
+        predict();
+    }
+  }
   updateDisplay('acc_x', event.acceleration.x);
-  updateDisplay('interval', event.interval);
+  updateDisplay('interval', event.interval, 3);
 }
-
-var sensors_started = false;
 
 function startSensors() {
   if (DeviceMotionEvent &&
@@ -31,7 +53,6 @@ function startSensors() {
     DeviceMotionEvent.requestPermission();
   }
   window.addEventListener("devicemotion", handleMotion);
-  sensors_started = true;
 }
 
 document.addEventListener('DOMContentLoaded', loadWam);
